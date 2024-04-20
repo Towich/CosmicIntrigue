56f7a3e4985ee9399d897fc402d7ae752c74ca8a
noAlxena <152198383+noAlxena@users.noreply.github.com>
2024-04-20 18:45:51 +0300

    Login ViewModel

--- app/src/main/java/com/towich/cosmicintrigue/ui/fragment/LoginFragment.kt
+++ app/src/main/java/com/towich/cosmicintrigue/ui/fragment/LoginFragment.kt
@@ -5,37 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.maps.SupportMapFragment
+import com.towich.cosmicintrigue.R
 import com.towich.cosmicintrigue.databinding.FragmentLoginBinding
+import com.towich.cosmicintrigue.ui.util.App
+import com.towich.cosmicintrigue.ui.viewmodel.LoginViewModel
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    val loginViewModel : LoginViewModel by viewModels{
+            (requireContext().applicationContext as App).viewModelFactory
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-/*
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        binding.proceed.setOnClickListener{
+            loginViewModel.SendLogin(binding.Login.text.toString())
+
         }
- */
     }
 
     override fun onDestroyView() {
